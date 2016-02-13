af1abc974688e64ff96d329f8327977862cb94e6
Jakub Konka <kubkon@jakubkonka.com>
2016-02-13 20:27:30 +0000

    First working version (very rough)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::collections::HashMap;
+use std::io;
+
+fn routes<'a>(words: &Vec<&'a str>) -> HashMap<&'a str, HashMap<usize, Vec<&'a str>>> {
+    let mut top_map = HashMap::with_capacity(words.len());
+    let n = words[0].len();
+    for &word in words {
+        let mut map = HashMap::with_capacity(n);
+        for i in 0 .. n {
+            map.insert(i, Vec::new());
+        }
+        top_map.insert(word, map);
+    }
+    for (k, v) in top_map.iter_mut(){
+        for &word in words {
+            if word.to_string() == k.to_string() {
+                continue;
+            }
+
+            let like = likeness(k, word);
+            v.get_mut(&like).unwrap().push(word);
+        }
+    }
+    top_map
+}
+
 fn likeness(word1: &str, word2: &str) -> usize {
     word1.chars().zip(word2.chars())
                  .filter(|&(c1, c2)| c1 == c2)
@@ -5,7 +31,38 @@ fn likeness(word1: &str, word2: &str) -> usize {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let words = vec![
+        "hates",
+        "unite",
+        "dried",
+        "thief",
+        "jokes",
+        "dazed",
+        "early",
+        "laser",
+        "basic",
+        "grief",
+        "noted",
+        "harsh",
+        "claim",
+        "crime",
+        "slide"
+    ];
+    // compute all routes
+    let mut r = routes(&words);
+    println!("Pick from: {:#?}", r.keys().collect::<Vec<&&str>>());
+
+    loop {
+        // user input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input);
+        let input: Vec<&str> = input.trim().split_whitespace().collect();
+        let word = input.get(0).unwrap();
+        let like = input.get(1).unwrap().parse::<usize>().unwrap();
+        // user selected "hates" and received likeness 2
+        r = routes(r.get(word).unwrap().get(&like).unwrap());
+        println!("Pick from: {:#?}", r.keys().collect::<Vec<&&str>>());
+    }
 }
 
 #[test]
